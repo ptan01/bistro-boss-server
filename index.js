a172504861d90a28aabe9f091c411a96fbc6f3db
ptan01 <ptanjim01@gmail.com>
2023-06-01 18:28:23 +0600

    menu delete api created

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-const verifyJWT = (req, res, next) =>{
-  const authorization = req.headers.authorization ;
-  if(!authorization){
-    return res.status(401).send({error : true , message: 'unauthorized access'})
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' })
   }
   const token = authorization.split(' ')[1]
-  jwt.verify(token, process.env.ASSESS_TOKEN_SECRET , (error, decoded)=>{
-    if(error){
-      return res.status(401).send({error : true , message: 'unauthorized access, filed to verify jwt token 😁'})
+  jwt.verify(token, process.env.ASSESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ error: true, message: 'unauthorized access, filed to verify jwt token 😁' })
     }
-    req.decoded = decoded ;
+    req.decoded = decoded;
     next()
   })
 }
@@ -49,26 +49,26 @@ async function run() {
     const cardCollection = client.db('bistroDb').collection('cards')
 
 
-// Warning: use verifyJWT before using verifyAdmin
-  const verifyAdmin = async(req, res, next)=>{
-    const email = req.decoded.email ;
-    const query = {email : email} ;
-    const user = await userCollection.findOne(query) ;
-    if(user?.role !== 'admin'){
-      return res.status(403).send({error: true , message : 'forbidden message'})
+    // Warning: use verifyJWT before using verifyAdmin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ error: true, message: 'forbidden message' })
+      }
+      next()
     }
-    next()
-  }
 
 
 
-  // json web token jwt api
+    // json web token jwt api
 
-  app.post('/jwt', (req, res)=>{
-    const user = req.body ;
-    const token = jwt.sign(user, process.env.ASSESS_TOKEN_SECRET, { expiresIn: '1h' }) ;
-    res.send({token})
-  })
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ASSESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token })
+    })
 
 
 
@@ -78,24 +78,24 @@ async function run() {
      * 0. do not show secure links to those who should not see the links
      * 1. use jwt token verifyJWT
      * 2. use verify admin middleware function 
-     * */ 
+     * */
 
-    app.get('/users',verifyJWT,verifyAdmin, async (req, res) => {
+    app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
       const result = await userCollection.find().toArray()
       res.send(result)
     })
 
 
-    app.get('/users/admin/:email', verifyJWT, async (req, res)=>{
-      const email = req.params?.email ;
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params?.email;
 
-      if(req.decoded.email !== email){
-       return res.send({admin: false})
+      if (req.decoded.email !== email) {
+        return res.send({ admin: false })
       }
 
-      const query = {email : email} ;
-      const user = await userCollection.findOne(query) ;
-      const result = {admin : user?.role === 'admin'} ;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const result = { admin: user?.role === 'admin' };
       res.send(result)
     })
 
@@ -118,7 +118,7 @@ async function run() {
           role: 'admin'
         },
       };
-      const result = await userCollection.updateOne(filter, updateDoc) ;
+      const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result)
     })
 
@@ -131,10 +131,20 @@ async function run() {
       res.send(result)
     })
 
-    app.post('/menu', verifyJWT, verifyAdmin, async(req, res)=>{
-      const menu = req.body ;
-      const result = await menuCollection.insertOne(menu); 
+    app.post('/menu', verifyJWT, verifyAdmin, async (req, res) => {
+      const menu = req.body;
+      const result = await menuCollection.insertOne(menu);
       res.send(result)
+    })
+
+   
+
+    app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result)
+      console.log(result)
     })
 
     app.get('/reviews', async (req, res) => {
@@ -149,9 +159,9 @@ async function run() {
       if (req.query?.email) {
         email = req.query.email;
       }
-      const decoded = req.decoded.email ;
-      if( email !== decoded){
-        return res.status(403).send({error : true , message: 'forbidden access, filed to verify jwt token email and access email 😁'})
+      const decoded = req.decoded.email;
+      if (email !== decoded) {
+        return res.status(403).send({ error: true, message: 'forbidden access, filed to verify jwt token email and access email 😁' })
       }
 
       const query = { email: email };
