28b0a4c8d3ed8ef8dbf12253ddf354aa1c7b311d
ptan01 <ptanjim01@gmail.com>
2023-06-01 17:18:58 +0600

    pust api created and route verify is jwt token and rerify admin

--- index.js
+++ index.js
@@ -131,6 +131,12 @@ async function run() {
       res.send(result)
     })
 
+    app.post('/menu', verifyJWT, verifyAdmin, async(req, res)=>{
+      const menu = req.body ;
+      const result = await menuCollection.insertOne(menu); 
+      res.send(result)
+    })
+
     app.get('/reviews', async (req, res) => {
       const result = await reviewCollection.find().toArray()
       res.send(result)
